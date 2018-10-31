eda087f39c2fa97b8e6b8b922824874326ad48af
Crone <crone1992@gmail.com>
2018-10-31 09:53:12 +0100

    Created model folder and Task class Task class should be updated for future use

---
+++ Ergogame/Ergogame/Model/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ergogame.Model
+{
+    class Task
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public bool Open { get; set; }
+        public Task(string name, DateTime date): this(name, date, true)
+        {
+        }
+        public Task(string name, DateTime date, bool open)
+        {
+            Name = name;
+            Date = date;
+            Open = open;
+        }
+    }
+}
