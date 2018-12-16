e64a56431ceed5100d4ac5ae95dbef1fa51a520e
Thomas Crone <crone1992@gmail.com>
2018-12-16 22:03:36 +0100

    Implemented complete exercise feature Works with added notes in DB

--- Ergogame/Ergogame/Model/UserModels/Note.cs
+++ Ergogame/Ergogame/Model/UserModels/Note.cs
@@ -7,7 +7,7 @@ using SQLiteNetExtensions.Attributes;
 namespace Ergogame.Model.UserModels
 {
     [Table("Notes")]
-    class Note
+    public class Note
     {
         [PrimaryKey, AutoIncrement, Column("_id")]
         public int Id { get; set; }
@@ -15,7 +15,17 @@ namespace Ergogame.Model.UserModels
         public int UserID { get; set; }
         [ForeignKey(typeof(Exercise))]
         public int ExerciseID { get; set; }
-        public string notes { get; set; }
+        public string Notes { get; set; }
         public DateTime CreateDate { get; set; }
+        public Note()
+        {
+        }
+        public Note(string notes, int user, int exercise)
+        {
+            CreateDate = DateTime.Now;
+            UserID = user;
+            ExerciseID = exercise;
+            Notes = notes;
+        }
     }
 }
